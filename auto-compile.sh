813b0edf16c3a9a38858e9516808c8382ed26a1a
whr94621 <whr94621@163.com>
2016-05-15 16:30:11 +0800

    add a script

---
+++ auto-compile.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+command -v pdflatex >/dev/null 2>&1 || {
+    echo >&2 "Tex Compiler not found.Pdflatex is necessary to compiler this doc."
+    exit 1
+}
+cd ../lectures/git_slides/ 
+
+rm *.pdf 2>/dev/null
+pdflatex git_for_coding_and_writing.tex
