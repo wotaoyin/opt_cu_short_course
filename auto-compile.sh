14729f2d0b0e4014de83ed32a72e0c4fd8830007
WEI HAORAN <whr94621@gmail.com>
2016-05-15 16:50:56 +0800

    Update auto-compile.sh

--- auto-compile.sh
+++ auto-compile.sh
@@ -4,7 +4,7 @@ command -v pdflatex >/dev/null 2>&1 || {
     echo >&2 "Tex Compiler not found.Pdflatex is necessary to compiler this doc."
     exit 1
 }
-cd ../lectures/git_slides/ 
+cd lectures/git_slides/ 
 
 rm *.pdf 2>/dev/null
 pdflatex git_for_coding_and_writing.tex
